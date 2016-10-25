ed9fce104babcf8fae23242f07bc065b20c91319
Nisanth Chunduru <nisanth074@gmail.com>
2016-10-25 22:43:54 +0530

    Fix typo. Also raise an exception in case a shell command fails.

--- newrelic_plugin_agent/plugins/postfix.py
+++ newrelic_plugin_agent/plugins/postfix.py
@@ -4,24 +4,32 @@ Postfix
 """
 from newrelic_plugin_agent.plugins import base
 import subprocess
+import logging
 
+LOGGER = logging.getLogger(__name__)
+
+class ShellCommandFailed(Exception):
+    """ Raised when a shell command fails """
 
 class Postfix(base.Plugin):
 
     GUID = 'com.supportbee.newrelic_postfix_agent'
 
     def poll(self):
-        self.add_gauge_value("Postfix/DeferredQueueSize", None, self.queue_size("deferred")) 
-        self.add_gauge_value("Postfix/BounceQueueSize", None, self.queue_size("bounce")) 
-        self.add_gauge_value("Postfix/HoldQueueSize", None, self.queue_size("hold"))
-        self.add_gauge_value("Postfix/CorruptQueueSize", None, self.queue_size("corrupt"))
-        self.add_gauge_value("Postfix/IncomingQueueSize", None, self.queue_size("incoming"))
-        self.add_gauge_value("Postfix/ActiveQueueSize", None, self.queue_size("active")) 
+        try:
+            self.add_gauge_value("Postfix/DeferredQueueSize", None, self.queue_size("deferred")) 
+            self.add_gauge_value("Postfix/BounceQueueSize", None, self.queue_size("bounce")) 
+            self.add_gauge_value("Postfix/HoldQueueSize", None, self.queue_size("hold"))
+            self.add_gauge_value("Postfix/CorruptQueueSize", None, self.queue_size("corrupt"))
+            self.add_gauge_value("Postfix/IncomingQueueSize", None, self.queue_size("incoming"))
+            self.add_gauge_value("Postfix/ActiveQueueSize", None, self.queue_size("active")) 
 
-        self.finish()
+            self.finish()
+        except ShellCommandFailed as e:
+            LOGGER.error(str(e))
 
     def queue_size(self, queue):
-        command = "find " + self.postfix_queue_directory() + "/" + queue + " type -f | wc -l"
+        command = "find " + self.postfix_queue_directory() + "/" + queue + " -type f | wc -l"
         queue_size = int(self.run_shell_command(command))
         return queue_size
 
@@ -33,4 +41,8 @@ class Postfix(base.Plugin):
     def run_shell_command(self, command):
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
         stdout_data, stderr_data = process.communicate()
+        if stderr_data:
+            error_message = "Running shell command `{0}` failed with error: `{1}`".format(command, stderr_data.rstrip())
+            raise ShellCommandFailed(error_message)
+
         return stdout_data.rstrip()
